048372a191cfed19529bdd7a75ceca04fec49bb1
AnamikaRajput06 <anamika@francium.tech>
2022-04-21 13:43:02 +0530

    Itegreted new and create actions for portfoiios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,24 @@ class PortfoliosController < ApplicationController
 def index
 	@portfolios_items = Portfolio.all
 	end
+def new
+	@portfolios_items =Portfolio.new
+end
+
+def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title,:subtitle,:body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: "Portfoliowas successfully created." }
+      else
+        format.html { render :new}
+        end
+    end
+  end
+
+
+
+
+
 end
