f4cc6e70419ae0e3895eabf2e460460338a0df42
AnamikaRajput06 <anamika@francium.tech>
2022-04-21 13:00:09 +0530

    Itegreted index action for portfoiio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,9 +16,9 @@ puts "5 skills created"
 puts 
 9.times do |port|
 	Portfolio.create!(
-		title:"Portfolio title no : #{port}",
+		title:"Portfolio title no : #{port+1}",
 		subtitle: "hello its going great",
-		body: "portfolio is good and its very useful........ iam learnin ruby on rails 5",
+		body: "portfolio is good and its very useful........ iam learning ruby on rails 5",
 		main_img: "https://via.placeholder.com/600x400",
 		thumb_image: "https://via.placeholder.com/300x150"
 		)
